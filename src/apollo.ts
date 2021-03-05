e5317677267e472644bba0990e98516f1be47a2c
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2021-03-05 22:42:20 +0900

    wss https backend uri

--- src/apollo.ts
+++ src/apollo.ts
@@ -16,7 +16,10 @@ export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "wss://uber-eats-clone-backend-51342.herokuapp.com/graphql"
+      : `ws://localhost:4000/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
@@ -26,7 +29,10 @@ const wsLink = new WebSocketLink({
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://uber-eats-clone-backend-51342.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
